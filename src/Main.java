4d845674842edb3c1a4993cbc6112ea30566baeb
Miroslav <138483137+MiroCoder@users.noreply.github.com>
2026-05-03 22:26:58 +0200

    Add habit search by name

---
+++ src/Main.java
@@ -0,0 +1,256 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+
+class Habit{
+    private String name;
+    private boolean completed;
+    private Priority priority;
+
+    public Habit(String name, boolean completed, Priority priority) {
+        this.name = name;
+        this.completed = completed;
+        this.priority = priority;
+    }
+
+    enum Priority {
+        High,
+        Medium,
+        Low
+
+    }
+
+    public void setName(String name) {
+        if (name!= null) {
+            this.name = name;
+        }
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public Priority getPriority() { return priority; }
+
+}
+
+class HabitService {
+
+    public static int calculateCompletion(ArrayList<Habit> habits) {
+        int counter = 0;
+        for (Habit h : habits) {
+            if (h.isCompleted()) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static Habit findHabit(ArrayList<Habit> habits, String name) {
+        for (Habit h : habits) {
+            if(h.getName().equalsIgnoreCase(name)){
+                return h;
+            }
+        }
+        return null;
+    }
+
+    public static String dayType(int habits_amount, int habits_done) {
+        double percent = (habits_done * 100.0) / habits_amount;
+
+        if (percent == 100.0){
+            return "Perfect day";
+        } else if (percent >= 70) {
+            return "Strong day";
+        } else if (percent >= 50) {
+            return "System day";
+        } else if (percent > 0) {
+            return "Recovery day";
+        } else {
+            return "Zero day";
+        }
+    }
+
+
+    public void sortByPriority(ArrayList<Habit> habits) {
+        habits.sort((h1,h2) -> h1.getPriority().ordinal() - h2.getPriority().ordinal());
+    }
+}
+
+class HabitPrinter{
+    public static void printHabitsByStatus(ArrayList<Habit> habits, boolean completed, String title) {
+        System.out.println(title);
+
+        boolean found = false;
+
+        for (Habit h : habits) {
+            if (h.isCompleted() == completed) {
+                System.out.println("- " + h.getName());
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("- none");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        HabitService service = new HabitService();
+        HabitPrinter printer = new HabitPrinter();
+
+
+        int amount = readAmount(sc);
+//        boolean[] bool = new boolean[ amount];
+//        String[] habits = new String[ amount];
+        ArrayList<Habit> habits = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            System.out.println("Enter the name of habit #" + (i + 1) + ": ");
+            String name = sc.nextLine();
+            Habit.Priority priority = readPriority(sc);
+
+            boolean completed = readComplete(sc, name);
+
+            Habit habit = new Habit(name, completed, priority);
+            habits.add(habit);
+//            habits[i] = sc.nextLine();
+//            bool[i] = readComplete(sc,habits[i]);
+//            String name = sc.nextLine();
+//            boolean bool = readComplete(sc,name);
+
+
+        }
+        //saveToFile(habits,bool);
+
+        int done = service.calculateCompletion(habits);
+//        System.out.println("Done habits: ");
+//        for (Habit h : habits) {
+//            if (h.isCompleted()){
+//                System.out.println(h.getName());
+//            }
+//        }
+//
+//        System.out.println("Not done habits: ");
+//        for (Habit h : habits) {
+//            if (!h.isCompleted()){
+//                System.out.println(h.getName());
+//            }
+//        }
+        service.sortByPriority(habits);
+        printer.printHabitsByStatus(habits, true, "Done habits: ");
+        printer.printHabitsByStatus(habits, false, "Not done habits: " + (amount - done));
+
+        System.out.println("Search habit by name: ");
+        String searchName = sc.nextLine();
+
+        Habit foundHabit = HabitService.findHabit(habits, searchName);
+        if (foundHabit != null){
+            System.out.println("Found: " + foundHabit.getName()
+            + "completed: " + foundHabit.isCompleted()
+            + "priority: " + foundHabit.getPriority());
+        } else {
+            System.out.println("Habit not found.");
+        }
+        System.out.println("You've completed " + done + " of " + amount +" habits (" + ((done * 100.0) / amount) + "%)");
+
+
+        System.out.println("You had a " + service.dayType(amount, done) + ".");
+
+        if (done == amount) {
+            System.out.println("You've completed all habits! Congratulations!");
+        } else {
+            System.out.println("You haven't completed " + (amount - done) + " habits yet");
+        }
+        sc.close();
+
+    }
+
+
+
+    public static int readAmount(Scanner sc){
+        int amount;
+        while (true) {
+            System.out.println("How many habits do you want to add: ");
+            if (sc.hasNextInt()){
+                amount = sc.nextInt();
+                sc.nextLine();
+
+                if (amount > 0){
+                    break;
+                } else {
+                    System.out.println("Please enter a number greater than 0.");
+                }
+            } else {
+                System.out.println("Invalid input. Enter a whole number.");
+                sc.nextLine();
+            }
+        }
+        return amount;
+    }
+
+    public static Habit.Priority readPriority(Scanner sc) {
+        while (true) {
+            System.out.println("Enter priority: low/medium/high: ");
+            String answer = sc.nextLine().trim().toLowerCase();
+
+            if (answer.equals("low")) {
+                return Habit.Priority.Low;
+
+            } else if (answer.equals("medium")) {
+                return Habit.Priority.Medium;
+            } else if (answer.equals("high")) {
+                return Habit.Priority.High;
+            } else {
+                System.out.println("Invalid input. Enter low, medium or high.");
+            }
+        }
+    }
+
+    public static boolean readComplete(Scanner sc, String habitName){
+
+        while (true) {
+            System.out.println("The " + habitName + " is done? yes/no");
+            String answer = sc.nextLine().trim().toLowerCase();
+
+            if (answer.equals("yes") || answer.equals("y")){
+                return true;
+            } else if (answer.equals("no") || answer.equals("n")) {
+                return false;
+            } else {
+                System.out.println("Invalid input.");
+            }
+        }
+    }
+
+//    public static void saveToFile(String[] habits, boolean[] bool) throws IOException {
+//        Path path = Path.of("habits.txt");
+//
+//        StringBuilder content = new StringBuilder();
+//
+//        for (int i = 0; i < habits.length; i++) {
+//            content.append(habits[i]).append(":").append(bool[i]).append("\n");
+//        }
+//
+//        Files.writeString(path, content.toString());
+//
+//    }
+
+
+
+
+}
